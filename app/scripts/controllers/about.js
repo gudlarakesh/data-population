8eee5c25448bcd9a663e29b0d1404b0a60c94dff
gudlarakesh <gudlarakesh1991@gmail.com>
2016-06-09 18:20:22 +0530

    working on angularjs using yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataPopulationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dataPopulationApp
+ */
+angular.module('dataPopulationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataPopulationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dataPopulationApp
+ */
+angular.module('dataPopulationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
